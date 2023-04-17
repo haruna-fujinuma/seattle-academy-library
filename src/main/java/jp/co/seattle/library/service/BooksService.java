398aa9264c0a0018dae29463ae7bcb0c1052333c
fujinumaharuna <fujinumaharuna@localhost>
2023-04-17 18:01:58 +0900

    task5_2次コミット

--- src/main/java/jp/co/seattle/library/service/BooksService.java
+++ src/main/java/jp/co/seattle/library/service/BooksService.java
@@ -33,7 +33,7 @@ public class BooksService {
 
 		// TODO 書籍名の昇順で書籍情報を取得するようにSQLを修正（タスク３）
 		List<BookInfo> getedBookList = jdbcTemplate.query(
-				"",
+				"SELECT * FROM books ORDER BY title ASC;",
 				new BookInfoRowMapper());
 
 		return getedBookList;
@@ -61,7 +61,7 @@ public class BooksService {
 	 */
 	public int registBook(BookDetailsInfo bookInfo) {
 		// TODO 取得した書籍情報を登録し、その書籍IDを返却するようにSQLを修正（タスク４）
-		String sql = "";
+		String sql = "INSERT INTO books(title, author, publisher, publish_date, isbn, description, thumbnail_url, thumbnail_name, reg_date, upd_date)VALUES(?,?,?,?,?,?,?,?,NOW(),NOW()) RETURNING id;";
 
 		int bookId = jdbcTemplate.queryForObject(sql, int.class, bookInfo.getTitle(), bookInfo.getAuthor(),
 				bookInfo.getPublisher(), bookInfo.getPublishDate(), bookInfo.getThumbnailName(),
@@ -89,12 +89,12 @@ public class BooksService {
 		String sql;
 		if (bookInfo.getThumbnailUrl() == null) {
 			// TODO 取得した書籍情報を更新するようにSQLを修正（タスク５）
-			sql = "UPDATE books SET title = ?,  author = ?,  publisher = ?, publish_date = ?,  isbn = ?, description = ?, reg_date = NOW(), upd_date = NOW() WHERE books.id = ?";
+			sql = "UPDATE books SET title = ?,  author = ?,  publisher = ?, publish_date = ?,  isbn = ?, description = ?,  upd_date = NOW() WHERE id = ?";
 			jdbcTemplate.update(sql, bookInfo.getTitle(), bookInfo.getAuthor(), bookInfo.getPublisher(),
 					bookInfo.getPublishDate(), bookInfo.getIsbn(), bookInfo.getDescription(), bookInfo.getBookId());
 		} else {
 			// TODO 取得した書籍情報を更新するようにSQLを修正（タスク５）
-			sql = "UPDATE books SET title = ?,  author = ?,  publisher = ?, publish_date = ?,  isbn = ?, description = ?, thumbnail_url = ?, thumbnail_name = ?, reg_date = NOW(), upd_date = NOW() WHERE books.id = ? ;";
+			sql = "UPDATE books SET title = ?,  author = ?,  publisher = ?, publish_date = ?, thumbnail_name = ?, thumbnail_url = ?, isbn = ?,  description = ?,  upd_date = NOW() WHERE id = ? ";
 			jdbcTemplate.update(sql, bookInfo.getTitle(), bookInfo.getAuthor(), bookInfo.getPublisher(),
 					bookInfo.getPublishDate(), bookInfo.getThumbnailName(), bookInfo.getThumbnailUrl(),
 					bookInfo.getIsbn(), bookInfo.getDescription(), bookInfo.getBookId());
